1b6a95f9fc369392820bbebf6a5983ba876e66d5
Kadyrov Alexander <alexander-kad@yandex.ru>
2015-08-23 23:38:23 +0500

    fixes

--- Test/Source/Classes/ViewController/Main/ViewController.h
+++ Test/Source/Classes/ViewController/Main/ViewController.h
@@ -1,5 +1,5 @@
 //
-//  AppDelegate.h
+//  ViewController.h
 //  Test
 //
 //  Created by Alexander on 23.08.15.
@@ -8,9 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
+@interface ViewController : UIViewController
 
 
 @end
---
+++ Test/Source/Classes/ViewController/Main/ViewController.m
@@ -0,0 +1,66 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by Alexander on 23.08.15.
+//  Copyright (c) 2015 Alexander Kadyrov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+#pragma mark -
+#pragma mark Initializate
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"";
+    
+    [self makeToolBar];
+    
+    [self makeItems];
+    
+    [self loadData];
+}
+
+#pragma mark -
+#pragma mark Make
+
+- (void)makeToolBar {
+    
+}
+
+- (void)makeItems {
+    
+}
+
+#pragma mark -
+#pragma mark Load
+
+- (void)loadData {
+    
+}
+
+#pragma mark -
+#pragma mark Set
+
+- (void)setData {
+    
+}
+
+#pragma mark -
+#pragma mark Actions
+
+#pragma mark -
+#pragma mark Other
+
+#pragma mark -
+#pragma mark Memory managment
+
+@end
