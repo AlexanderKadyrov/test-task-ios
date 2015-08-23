18bf556556c981dab9a785075b8c6e050d03891e
Kadyrov Alexander <alexander-kad@yandex.ru>
2015-08-23 23:37:28 +0500

    reate

---
+++ Test/Source/Shared/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Test
+//
+//  Created by Alexander on 23.08.15.
+//  Copyright (c) 2015 Alexander Kadyrov. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    
+    
+    return YES;
+}
+
+@end
