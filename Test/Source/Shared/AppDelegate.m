fe384de1da8d6989f2794d108e030c26df88d220
Kadyrov Alexander <alexander-kad@yandex.ru>
2015-08-23 23:57:07 +0500

    develop

--- Test/Source/Shared/AppDelegate.m
+++ Test/Source/Shared/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     return YES;
 }
